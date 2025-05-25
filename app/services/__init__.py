7ba32d3fe4e4790f183d90cd4419d9e59ff1fbae
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-05-24 22:55:01 -0300

    Se creo el servicio de departamento y se agregó al init DepartamentoService

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,3 +2,4 @@ from .facultad_service import FacultadService
 from .alumno_service import AlumnoService
 from .tipodocumento_service import TipoDocumentoService
 from .grado_service import GradoService
+from .departamento_service import DepartamentoService
