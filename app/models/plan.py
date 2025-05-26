3be23426f15f8295da1feb67406d35608ef50d1c
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-05-26 20:07:50 -0300

    Se modificó fecha_inicio y fecha_fin de string a entero

--- app/models/plan.py
+++ app/models/plan.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 @dataclass(init=False, repr=True, eq=True)
 class Plan():
     nombre: str
-    fecha_inicio: str
-    fecha_fin: str
+    fecha_inicio: int
+    fecha_fin: int
     observacion: str
