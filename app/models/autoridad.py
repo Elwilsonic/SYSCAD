0551e4625394dfc652c21eb8e3b39b7faa5d4e8c
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-05-28 17:49:14 -0300

    Refactor: Convertido el modelo Autoridad a modelo SQLAlchemy

--- app/models/autoridad.py
+++ app/models/autoridad.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
-from app.models.cargo import Cargo
+from app.models import Cargo
+
+from app import db
 
 @dataclass(init=False, repr=True, eq=True)
-class Autoridad():
-    nombre: str
-    cargo: Cargo
-    telefono: str
-    email: str
+class Autoridad(db.Model):
+    __tablename__ = 'autoridades'
+    id: int = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    nombre: str = db.Column(db.String(100), nullable=False)
+    telefono: str = db.Column(db.String(20), nullable=True)
+    email: str = db.Column(db.String(100), nullable=True)
+
+    cargo_id: int = db.Column(db.Integer, db.Foreignkey('cargos.id'))
+    cargo = db.relationship('Cargo')
