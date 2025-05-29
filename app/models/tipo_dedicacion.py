0047e98aa495c3823af6abacc374b501c2a60211
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-05-28 22:46:44 -0300

    Se corrigió la relación entre Cargo y TipoDedicacion (nombre correcto de la tabla 'tipos_dedicacion' en ForeignKey)

--- app/models/tipo_dedicacion.py
+++ app/models/tipo_dedicacion.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from app import db
 
 @dataclass(init=False, repr=True, eq=True)
-class TipoDedicacion:
-    nombre: str
-    observacion: str
-    
-    
+class TipoDedicacion(db.Model):
+    __tablename__ = 'tipo_dedicacion'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    observacion = db.Column(db.String(255), nullable=True)
