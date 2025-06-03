bb21a2c60d90fc5f67ffdc13c5e28e2c3d8180d9
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-06-03 17:59:21 -0300

    Se agregaron pruebas CRUD completas para el modelo Area en test_area.py

--- test/test_area.py
+++ test/test_area.py
@@ -3,6 +3,7 @@ import os
 from flask import current_app
 from app import create_app
 from app.models.area import Area
+from app import db
 
 class AreaTestCase(unittest.TestCase):
     def setUp(self):
@@ -10,8 +11,11 @@ class AreaTestCase(unittest.TestCase):
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
+        db.session.remove()
+        db.drop_all()
         self.app_context.pop()
 
     def test_area_creation(self):
@@ -20,3 +24,43 @@ class AreaTestCase(unittest.TestCase):
         self.assertIsNotNone(area)
         self.assertIsNotNone(area.nombre)
         self.assertEqual(area.nombre, "matematica")
+    
+    def test_crear_area(self):
+        area = Area(nombre="Matemática")
+        db.session.add(area)
+        db.session.commit()
+        self.assertIsNotNone(area.id)
+        self.assertEqual(area.nombre, "Matemática")
+
+    def test_buscar_area(self):
+        area = Area(nombre="Matemática")
+        db.session.add(area)
+        db.session.commit()
+        encontrada = Area.query.get(area.id)
+        self.assertIsNotNone(encontrada)
+        self.assertEqual(encontrada.nombre, "Matemática")
+    
+    def test_buscar_todos(self):
+        db.session.add(Area(nombre="Matemática"))
+        db.session.add(Area(nombre="Ingles"))
+        db.session.commit()
+        areas = Area.query.all()
+        self.assertGreaterEqual(len(areas), 2)
+    
+    def test_actualizar_area(self):
+        area = Area(nombre="Matemática")
+        db.session.add(area)
+        db.session.commit()
+        area.nombre = "Matemáticas Aplicadas"
+        db.session.commit()
+        actualizada = Area.query.get(area.id)
+        self.assertEqual(actualizada.nombre, "Matemáticas Aplicadas")
+    
+    def test_borrar_area(self):
+        area = Area(nombre="Temporal")
+        db.session.add(area)
+        db.session.commit()
+        db.session.delete(area)
+        db.session.commit()
+        eliminada = Area.query.get(area.id)
+        self.assertIsNone(eliminada)
