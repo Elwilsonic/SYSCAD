ec18cc1017605ac938d90d55205f18d43acd5d8a
martinabatista <martibatista2005@gmail.com>
2025-06-04 20:11:03 -0300

    se arreglo _tablename_='materias' a __tablename__ = 'materisas'

--- app/models/materia.py
+++ app/models/materia.py
@@ -1,9 +1,9 @@
 from app import db
 
 class Materia(db.Model):
-    _tablename_ = 'materias'
+    __tablename__ = 'materias'
 
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     codigo = db.Column(db.String(50), nullable=False)
-    observacion = db.Column(db.String(255))
+    observacion = db.Column(db.String(255), nullable=True)
