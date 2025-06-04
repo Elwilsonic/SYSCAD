5ee4d4c211705a649c2970d28c4cdbd3f7bc4d3b
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-06-03 21:03:49 -0300

    Se modificó el  modelo Area como entidad de SQLAlchemy

--- app/models/area.py
+++ app/models/area.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
+from app import db
 
 @dataclass(init=False, repr=True, eq=True)
-class Area():
-    nombre: str
+class Area(db.Model):
+    __tablename__ = 'areas'
+
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre: str = db.Column(db.String(100), nullable=False)
+
