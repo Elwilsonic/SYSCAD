da1cf24860d43cf4cb0c55f77c9b31887fb7d77b
Leandro <leacardozo2002@gmail.com>
2025-06-04 01:04:18 -0300

    se creo el service de grupo

---
+++ app/services/grupo_service.py
@@ -0,0 +1,55 @@
+from app.models import Grupo
+from app.repositories import GrupoRepository
+
+class GrupoService:
+
+    @staticmethod
+    def crear_grupo(grupo: Grupo):
+        """
+        Crea un nuevo grupo en la base de datos.
+        :param grupo: Objeto Grupo a crear.
+        :return: Objeto Grupo creado.
+        """
+        return GrupoRepository.crear(grupo)
+
+    @staticmethod
+    def buscar_por_id(id: int) -> Grupo:
+        """
+        Busca un grupo por su ID.
+        :param id: ID del grupo a buscar.
+        :return: Objeto Grupo encontrado o None si no se encuentra.
+        """
+        return GrupoRepository.buscar_por_id(id)
+
+    @staticmethod
+    def buscar_todos() -> list[Grupo]:
+        """
+        Busca todos los grupos en la base de datos.
+        :return: Lista de objetos Grupo.
+        """
+        return GrupoRepository.buscar_todos()
+
+    @staticmethod
+    def actualizar_grupo(grupo: Grupo):
+        """
+        Actualiza un grupo en la base de datos.
+        :param grupo: Objeto Grupo a actualizar.
+        :return: Objeto Grupo actualizado o None si no se encuentra.
+        """
+        grupo_existente = GrupoRepository.buscar_por_id(grupo.id)
+        if not grupo_existente:
+            return None
+        grupo_existente.nombre = grupo.nombre
+        return grupo_existente
+
+    @staticmethod
+    def borrar_por_id(id: int) -> Grupo:
+        """
+        Borra un grupo por su ID.
+        :param id: ID del grupo a borrar.
+        :return: Objeto Grupo borrado o None si no se encuentra.
+        """
+        grupo = GrupoRepository.borrar_por_id(id)
+        if not grupo:
+            return None
+        return grupo
