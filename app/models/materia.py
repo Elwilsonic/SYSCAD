40b83af22c4de7b8dd92746a9432354e76a66272
CabezaFlorncia <florenciacabeza2109@gmail.com>
2025-06-04 17:29:29 -0300

    se modifico el modelo de materia como entidad de SQLAlchemy

--- app/models/materia.py
+++ app/models/materia.py
@@ -1,7 +1,9 @@
-from dataclasses import dataclass
+from app import db
 
-@dataclass(init=False, repr=True, eq=True)
-class Materia():
-    nombre: str
-    codigo: str
-    observacion: str
+class Materia(db.Model):
+    _tablename_ = 'materias'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    codigo = db.Column(db.String(50), nullable=False)
+    observacion = db.Column(db.String(255))
