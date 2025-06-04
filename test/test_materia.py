0958a1bfcaa074bf5afa227bb62927a5250f55f0
CabezaFlorncia <florenciacabeza2109@gmail.com>
2025-06-04 17:36:04 -0300

    se actualizo el test de materia para usar el CRUD completo

--- test/test_materia.py
+++ test/test_materia.py
@@ -1,8 +1,9 @@
 import unittest
 import os
 from flask import current_app
-from app import create_app
-from app.models.materia import Materia
+from app import create_app,db
+from app.models import Materia
+from app.services import MateriaService
 
 class MateriaTestCase(unittest.TestCase):
     def setUp(self):
@@ -10,17 +11,18 @@ class MateriaTestCase(unittest.TestCase):
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
+        db.session.remove()
+        db.drop_all()
         self.app_context.pop()
 
-    def test_materia_creation(self):
-        materia = Materia()
-        materia.nombre = "Matematicas"
-        materia.codigo = "MAT101"
-        materia.observacion = "Observacion de prueba"
-        self.assertIsNotNone(materia)
-        self.assertIsNotNone(materia.nombre)
-        self.assertEqual(materia.nombre, "Matematicas")
-        self.assertEqual(materia.codigo, "MAT101")
-        self.assertEqual(materia.observacion, "Observacion de prueba")
+    def test_crear_materia(self):
+        materia = Materia(nombre="Matemáticas", codigo="MAT101", observacion="Observación de prueba")
+        resultado = MateriaService.crear(materia)
+
+        self.assertIsNotNone(resultado.id)
+        self.assertEqual(resultado.nombre, "Matemáticas")
+        self.assertEqual(resultado.codigo, "MAT101")
+        self.assertEqual(resultado.observacion, "Observación de prueba")
