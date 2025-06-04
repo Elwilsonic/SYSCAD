42edf93d24060f8eb233b3c56ee56d637c5778b0
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-06-04 12:12:18 -0300

    Se modificó el modelo plan con integración a SQLAlchemy

--- app/models/plan.py
+++ app/models/plan.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from app import db
 
 @dataclass(init=False, repr=True, eq=True)
-class Plan():
-    nombre: str
-    fecha_inicio: int
-    fecha_fin: int
-    observacion: str
+class Plan(db.Model):
+    __tablename__ = 'planes'
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre: str = db.Column(db.String(100), nullable=False)
+    fecha_inicio: str = db.Column(db.String(20), nullable=False)
+    fecha_fin: str = db.Column(db.String(20), nullable=False)
+    observacion: str = db.Column(db.String(255))
