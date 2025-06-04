22000f40d1b04184b7047950ef1cf5345b570e32
Leandro <leacardozo2002@gmail.com>
2025-06-04 00:57:27 -0300

    se modifico el modelo de grupo

--- app/models/grupo.py
+++ app/models/grupo.py
@@ -1,7 +1,11 @@
-
 from dataclasses import dataclass
+from app import db
 
 @dataclass(init=False, repr=True, eq=True)
-class Grupo():
-    nombre: str
+class Grupo(db.Model):
+    _tablename_ = 'grupos'
+
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre: str = db.Column(db.String(100), nullable=False)
+    
     
