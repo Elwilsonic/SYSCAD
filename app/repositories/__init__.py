6e4479d63d4e880b68982ca9c4e7c6ef614d0afb
Lucas <lucasarrece@gmail.com>
2025-06-07 12:42:03 -0300

    Se modifico el Init en repositorio

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -12,3 +12,4 @@ from .plan_repositorio import PlanRepository
 from .materia_repositorio import MateriaRepository
 from .orientacion_repositorio import OrientacionRepository
 from .autoridad_repositorio import AutoridadRepository
+from .cargo_repositorio import CargoRepository
