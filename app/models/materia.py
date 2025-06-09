e3d3d4fe1bdd7c684de897efc8796013da3d391c
martinabatista <martibatista2005@gmail.com>
2025-06-09 10:58:46 -0300

    se actualizo el modelo Materia: se agrega relación con Orientacion y métodos para asociar autoridades

--- app/models/materia.py
+++ app/models/materia.py
@@ -1,11 +1,24 @@
+from dataclasses import dataclass
 from app import db
+from app.models import autoridades_materias
 
+@dataclass(init=False, repr=True, eq=True)
 class Materia(db.Model):
     __tablename__ = 'materias'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    codigo = db.Column(db.String(50), nullable=False)
-    observacion = db.Column(db.String(255), nullable=True)
-    # Relación inversa para la relación muchos a muchos con Autoridad
-    autoridades = db.relationship('Autoridad', secondary='autoridades_materias', back_populates='materias')
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre: str = db.Column(db.String(100), nullable=False)
+    codigo: str = db.Column(db.String(50), nullable=False)
+    observacion: str = db.Column(db.String(255), nullable=True)
+    
+    orientacion_id: int = db.Column(db.Integer, db.ForeignKey('orientaciones.id'))
+    orientacion = db.relationship('Orientacion')
+    # Relación muchos a muchos con Autoridad
+    autoridades = db.relationship('Autoridad', secondary=autoridades_materias, back_populates='materias')
+    
+    def asociar_autoridad(self, autoridad):
+        if autoridad not in self.autoridades:
+            self.autoridades.append(autoridad)
+    
+    def desasociar_autoridad(self, autoridad):
+        if autoridad in self.autoridades:
+            self.autoridades.remove(autoridad)
