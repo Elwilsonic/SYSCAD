f7188a9c37bf6859641e6dd630e5f0a1dfe2de32
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-06-19 23:50:48 -0300

    Se creó el modelo de nota para gestionar las notas de los alumnos

---
+++ app/models/nota.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from app import db
+
+@dataclass(init=False, repr=True, eq=True)
+class Nota(db.Model):
+    __tablename__ = 'notas'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    valor = db.Column(db.Float, nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    alumno_id = db.Column(db.Integer, db.ForeignKey('alumnos.id'), nullable=False)
+    alumno = db.relationship('Alumno', back_populates='notas')
