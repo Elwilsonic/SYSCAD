ebacee926365a8c427e6643a96cb3df53cb59d22
Leandro <leacardozo2002@gmail.com>
2025-08-12 20:59:01 -0300

    se creo el mapping de CategoriaCargo

---
+++ app/mapping/categoriacargo_mapping.py
@@ -0,0 +1,10 @@
+from marshmallow import fields, Schema, post_load, validate
+from app.models import CategoriaCargo
+
+class CategoriaCargoMapping(Schema):
+    id = fields.Integer(dump_only=True)
+    nombre = fields.String(required=True, validate=validate.Length(min=1, max=50))
+
+    @post_load
+    def nueva_categoria(self, data, **kwargs) -> CategoriaCargo:
+        return CategoriaCargo(**data)
