5e636bd89deae19a2362011a5bda03a14311a20f
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-08-12 21:34:34 -0300

    Se agregó el resource de tipo especialidad

---
+++ app/resources/tipo_especialidad_resources.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from app.mapping import TipoEspecialidadMapping
+from app.services import TipoEspecialidadService
+
+tipo_especialidad_bp = Blueprint('tipo_especialidad', __name__)
+tipo_especialidad_mapping = TipoEspecialidadMapping()
+
+@tipo_especialidad_bp.route('/tipo_especialidad', methods=['GET'])
+def buscar_todos():
+    tipo_especialidades = TipoEspecialidadService.buscar_todos()
+    return tipo_especialidad_mapping.dump(tipo_especialidades, many=True), 200
+
+@tipo_especialidad_bp.route('/tipo_especialidad/<int:id>', methods=['GET'])
+def buscar_por_id(id):
+    tipo_especialidad = TipoEspecialidadService.buscar_por_id(id)
+    return tipo_especialidad_mapping.dump(tipo_especialidad), 200
+
+@tipo_especialidad_bp.route('/tipo_especialidad', methods=['POST'])
+def crear():
+    tipo_especialidad = tipo_especialidad_mapping.load(request.get_json())
+    TipoEspecialidadService.crear(tipo_especialidad)
+    return jsonify("TipoEspecialidad creada exitosamente"), 200
+
+@tipo_especialidad_bp.route('/tipo_especialidad/<int:id>', methods=['DELETE'])
+def borrar_por_id(id):
+    TipoEspecialidadService.borrar_por_id(id)
+    return jsonify("TipoEspecialidad borrada exitosamente"), 200
