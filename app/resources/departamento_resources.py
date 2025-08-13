1e4d287b3ad2eed71d1df770ae0dbddcc8ba3e85
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-08-12 21:36:54 -0300

    Se agregó el resource de departamento

---
+++ app/resources/departamento_resources.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from app.mapping import DepartamentoMapping
+from app.services import DepartamentoService
+
+departamento_bp = Blueprint('departamento', __name__)
+departamento_mapping = DepartamentoMapping()
+
+@departamento_bp.route('/departamento', methods=['GET'])
+def buscar_todos():
+    departamentos = DepartamentoService.buscar_todos()
+    return departamento_mapping.dump(departamentos, many=True), 200
+
+@departamento_bp.route('/departamento/<int:id>', methods=['GET'])
+def buscar_por_id(id):
+    departamento = DepartamentoService.buscar_por_id(id)
+    return departamento_mapping.dump(departamento), 200
+
+@departamento_bp.route('/departamento', methods=['POST'])
+def crear():
+    departamento = departamento_mapping.load(request.get_json())
+    DepartamentoService.crear(departamento)
+    return jsonify("Departamento creado exitosamente"), 200
+
+@departamento_bp.route('/departamento/<int:id>', methods=['DELETE'])
+def borrar_por_id(id):
+    DepartamentoService.borrar_por_id(id)
+    return jsonify("Departamento borrado exitosamente"), 200
