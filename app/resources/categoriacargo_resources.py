6698e8e937b61007826756a7a8896197e4ffcb3c
martinabatista <martibatista2005@gmail.com>
2025-08-12 22:39:18 -0300

    se arreglo el codigo de categoriacargo_resources

--- app/resources/categoriacargo_resources.py
+++ app/resources/categoriacargo_resources.py
@@ -1,27 +1,27 @@
-from flask import jsonify, Blueprint, request
-from app.mapping.autoridad_mapping import AutoridadMapping
-from app.services.autoridad_service import AutoridadService
+from flask import Blueprint, jsonify, request
+from app.mapping import CategoriaCargoMapping
+from app.services import CategoriaCargoService
 
-autoridad_bp = Blueprint('autoridad', __name__)
-autoridad_mapping = AutoridadMapping()
+categoriacargo_bp = Blueprint('categoriacargo', __name__)
+categoriacargo_mapping = CategoriaCargoMapping()
 
-@autoridad_bp.route('/autoridad', methods=['GET'])
+@categoriacargo_bp.route('/categoriacargo', methods=['GET'])
 def buscar_todos():
-    autoridades = AutoridadService.buscar_todos()
-    return autoridad_mapping.dump(autoridades, many=True), 200
-    
-@autoridad_bp.route('/autoridad/<int:id>', methods=['GET'])
+    categoriacargos = CategoriaCargoService.buscar_todos()
+    return categoriacargo_mapping.dump(categoriacargos, many=True), 200
+
+@categoriacargo_bp.route('/categoriacargo/<int:id>', methods=['GET'])
 def buscar_por_id(id):
-    autoridad = AutoridadService.buscar_por_id(id)
-    return autoridad_mapping.dump(autoridad), 200
+    categoriacargo = CategoriaCargoService.buscar_por_id(id)
+    return categoriacargo_mapping.dump(categoriacargo), 200
 
-@autoridad_bp.route('/autoridad', methods=['POST'])
+@categoriacargo_bp.route('/categoriacargo', methods=['POST'])
 def crear():
-    autoridad = autoridad_mapping.load(request.get_json())
-    AutoridadService.crear(autoridad) 
-    return jsonify("Autoridad creada exitosamente"), 200
+    categoriacargo = categoriacargo_mapping.load(request.get_json())
+    CategoriaCargoService.crear(categoriacargo)
+    return jsonify("CategoriaCargo creada exitosamente"), 200
 
-@autoridad_bp.route('/autoridad/<int:id>', methods=['DELETE'])
+@categoriacargo_bp.route('/categoriacargo/<int:id>', methods=['DELETE'])
 def borrar_por_id(id):
-    AutoridadService.borrar_por_id(id)
-    return jsonify("Autoridad borrada exitosamente"), 200
+    CategoriaCargoService.borrar_por_id(id)
+    return jsonify("CategoriaCargo borrada exitosamente"), 200
