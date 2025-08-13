0780b80a973c11d0ed18de2ed3ba900e6d640169
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-08-12 21:33:55 -0300

    Se agregó el resource de tipo de dedicacion

---
+++ app/resources/tipodedicacion_resources.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, jsonify, request
+from app.mapping import TipoDedicacionMapping
+from app.services import TipoDedicacionService
+
+tipodedicacion_bp = Blueprint('tipodedicacion', __name__)
+tipodedicacion_mapping = TipoDedicacionMapping()
+
+@tipodedicacion_bp.route('/tipodedicacion', methods=['GET'])
+def buscar_todos():
+    tipodedicaciones = TipoDedicacionService.buscar_todos()
+    return tipodedicacion_mapping.dump(tipodedicaciones, many=True), 200
+
+@tipodedicacion_bp.route('/tipodedicacion/<int:id>', methods=['GET'])
+def buscar_por_id(id):
+    tipodedicacion = TipoDedicacionService.buscar_por_id(id)
+    if tipodedicacion:
+        return tipodedicacion_mapping.dump(tipodedicacion), 200
+    return jsonify({'error': 'No encontrado'}), 404
+
+@tipodedicacion_bp.route('/tipodedicacion', methods=['POST'])
+def crear():
+    data = request.get_json()
+    errors = tipodedicacion_mapping.validate(data)
+    if errors:
+        return jsonify(errors), 400
+    tipodedicacion = TipoDedicacionService.crear(data)
+    return tipodedicacion_mapping.dump(tipodedicacion), 201
+
+@tipodedicacion_bp.route('/tipodedicacion/<int:id>', methods=['DELETE'])
+def borrar_por_id(id):
+    exito = TipoDedicacionService.borrar_por_id(id)
+    if exito:
+        return jsonify({'mensaje': 'TipoDedicacion borrada exitosamente'}), 200
+    return jsonify({'error': 'No encontrado'}), 404
