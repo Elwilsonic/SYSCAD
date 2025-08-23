a0af669305675ad8f80b8c4ab49324e1b16a1ab0
Leandro <leacardozo2002@gmail.com>
2025-08-23 20:57:26 -0300

    se agregaron los datos necesarios en el mapping para autoridad

--- app/mapping/autoridad_mapping.py
+++ app/mapping/autoridad_mapping.py
@@ -1,7 +1,13 @@
+from marshmallow import fields, Schema, post_load, validate
 from app.models.autoridad import Autoridad
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-class AutoridadMapping(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Autoridad
-        load_instance = True
+class AutoridadMapping(Schema):
+    id = fields.Integer(dump_only=True)
+    nombre = fields.String(required=True, validate=validate.Length(min=1, max=100))
+    email = fields.Email(required=True, validate=validate.Length(max=100))
+    telefono = fields.String(validate=validate.Length(max=20))
+
+    @post_load
+    def nueva_autoridad(self, data, **kwargs) -> Autoridad:
+        return Autoridad(**data)
+    
