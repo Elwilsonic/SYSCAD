0c2bc2fc7bda7df87d24392f8d72baeb6b3d0431
Leandro <leacardozo2002@gmail.com>
2025-08-23 20:58:11 -0300

    se agregaron los datos necesarios en el mapping para facultad

--- app/mapping/facultad_mapping.py
+++ app/mapping/facultad_mapping.py
@@ -1,7 +1,14 @@
+from marshmallow import Schema, fields, post_load, validate
 from app.models.facultad import Facultad
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-class FacultadMapping(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Facultad
-        load_instance = True
+class FacultadMapping(Schema):
+    id = fields.Integer(dump_only=True)
+    nombre = fields.String(required=True, validate=validate.Length(min=1, max=100))
+    abreviatura = fields.String(required=True, validate=validate.Length(min=1, max=10))
+    directorio = fields.String(required=True, validate=validate.Length(min=1, max=100))
+    sigla = fields.String(required=True, validate=validate.Length(min=1, max=10))
+    email = fields.Email(required=True, validate=validate.Length(max=100))
+
+    @post_load
+    def nueva_facultad(self, data, **kwargs) -> Facultad:
+        return Facultad(**data)
