97c20dca47fe1453951d16aeaa8a230fbccd6c02
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-08-23 18:08:38 -0300

    Se dejó el hashids para mayor seguridad del ID

--- app/mapping/universidad_mapping.py
+++ app/mapping/universidad_mapping.py
@@ -3,7 +3,6 @@ from app.models import Universidad
 
 class UniversidadMapping(Schema):
     hashids = fields.String(dump_only=True)
-    id = fields.Integer() 
     nombre = fields.String(required = True, validate = validate.Length(min=1, max=100))
     sigla = fields.String(required = True, validate = validate.Length(min=1, max=10))
 
