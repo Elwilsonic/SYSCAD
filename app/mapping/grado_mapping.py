13420ac18f8c5eaea4b9842ae65c1a9108964c9e
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-08-23 22:34:36 -0300

    Se agregaron nuevos campos de validación

--- app/mapping/grado_mapping.py
+++ app/mapping/grado_mapping.py
@@ -4,6 +4,7 @@ from app.models import Grado
 class GradoMapping(Schema):
     id = fields.Integer(dump_only=True)
     nombre = fields.String(required=True, validate=validate.Length(min=1, max=50))
+    descripcion = fields.String(required=True, validate=validate.Length(min=1, max=200))
 
     @post_load
     def nuevo_grado(self, data, **kwargs) -> Grado:
