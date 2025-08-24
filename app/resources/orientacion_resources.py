ab59db4f90172b0693612aafb7b72743e830e92a
Leandro <leacardozo2002@gmail.com>
2025-08-24 00:16:50 -0300

    se creo el resources de Orientacion

---
+++ app/resources/orientacion_resources.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from app.mapping import OrientacionMapping
+from app.services import OrientacionService
+
+orientacion_bp = Blueprint('orientacion', __name__)
+orientacion_mapping = OrientacionMapping()
+
+@orientacion_bp.route('/orientaciones', methods=['GET'])
+def buscar_todos():
+    orientaciones = OrientacionService.buscar_todos()
+    return jsonify(orientacion_mapping.dump(orientaciones)), 200
+
+@orientacion_bp.route('/orientaciones/<int:id>', methods=['GET'])
+def buscar_por_id(id):
+    orientacion = OrientacionService.buscar_por_id(id)
+    return jsonify(orientacion_mapping.dump(orientacion)), 200
+
+@orientacion_bp.route('/orientaciones', methods=['POST'])
+def crear():
+    orientacion = orientacion_mapping.load(request.get_json())
+    OrientacionService.crear(orientacion)
+    return jsonify("Orientación creada exitosamente"), 200
+
+@orientacion_bp.route('/orientaciones/<int:id>', methods=['DELETE'])
+def borrar_por_id(id):
+    OrientacionService.borrar_por_id(id)
+    return jsonify("Orientación eliminada exitosamente"), 200
